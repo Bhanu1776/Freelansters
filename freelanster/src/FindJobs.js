fbbc242b0a5f11826b0290cb75eecc5ff9fa9434
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-09-30 11:35:04 +0530

    Misc: Cleaned up js

--- freelanster/src/FindJobs.js
+++ freelanster/src/FindJobs.js
@@ -7,13 +7,6 @@ import Footer from './Components/Footer';
 
 const FindJobs = () => {
 
-    //* Logic for implementing the navbar
-
-    window.addEventListener('scroll', function () {
-        var navbar = document.querySelector('#navbar');
-        navbar.classList.toggle("sticky", window.scrollY > 0)
-    })
-
     return (
         <>
             <Navbar />
