39dd298d86ccafcb4d45c4a704efade5fe966644
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-09-30 11:36:10 +0530

    Misc: Cleaned up Modules and components

--- freelanster/src/index.js
+++ freelanster/src/index.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
+// import Login from './Login';
 import FindJobs from './FindJobs';
-import Login from './Login';
-// import FindJobs from './FindJobs'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    {/* <Navbar /> */}
-    {/* <App /> */}
-    {/* <Footer /> */}
+    <App />
+    {/* <Login /> */}
 
     {/* <FindJobs /> */}
 
-    <Login />
 
   </>
 
