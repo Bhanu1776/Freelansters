8e8094fc596d7ce34cbeaf6aa8bd68f225d9eb02
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-10-08 23:30:59 +0530

    Update: Initial implementation of middleware in express

--- freelanster/Backend/App.js
+++ freelanster/Backend/App.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 
+const middleware = (req, res, next) => {
+    console.log('Hello Im a middleware');
+    next();
+}
+
 app.get('/', (req, res) => {
     res.send('Hello world from the server');
 })
 app.get('/Login', (req, res) => {
     res.send('Hello world from the Login server');
 })
-app.get('/FindJobs', (req, res) => {
+app.get('/FindJobs', middleware, (req, res) => {
     res.send('Hello world from the FJ server');
 })
 app.get('/FindFreelancer', (req, res) => {
