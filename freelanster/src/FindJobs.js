09f017eba5c4a2ade831025e8d030fefbcdd1363
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-10-25 23:19:39 +0530

    Added: Login authentication Now, Login is required to access this page email_id = osama@gmail.com pass = osama

--- freelanster/src/FindJobs.js
+++ freelanster/src/FindJobs.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
 import Filter from './Components/Filter';
@@ -6,6 +7,39 @@ import Footer from './Components/Footer';
 
 
 const FindJobs = () => {
+
+    const navigate = useNavigate();
+
+    const callFJ = async () => {
+
+        try {
+            const res = await fetch('/Findjobs', {
+                method: "GET",                        //! IMP
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                credentials: "include"
+            });
+
+            const data = await res.json();
+            console.log(data);
+
+            if (!res.status === 200) {
+                const error = new Error(res.error);
+                throw error;
+            }
+        } catch (err) {
+            console.log(err);
+            navigate('/Login')
+        }
+    }
+
+    useEffect(() => {
+        callFJ();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
 
     return (
         <>
