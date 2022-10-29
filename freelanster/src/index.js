c517be0356c535c65ccf3c8823ecc0faa081cc48
Osama <shaikhosama499@gmail.com>
2022-10-29 23:13:17 +0530

    Update: Implemented Style Components GlobalStyle file imported

--- freelanster/src/index.js
+++ freelanster/src/index.js
@@ -2,31 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "swiper/css/bundle";
-import App from './App';
+import { GlobalStyle } from './Styles/GlobalStyle';
+// import App from './App';
 // import About from './Components/About';
 import Login from './Login';
 // import FindJobs from './FindJobs';
-import Profile from './Components/Profile';
+// import Profile from './Components/Profile';
 // import Error from './Components/Error';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
+  <GlobalStyle/>
     <BrowserRouter>
       <Routes>
-        <Route exact path='/' element={<App />}></Route>
+        {/* <Route exact path='/' element={<App />}></Route>
         <Route path='/Home' element={<App />}></Route>
         <Route path='Login' element={<Login />}></Route>
-        {/* <Route path='/FindJobs' element={<FindJobs />}></Route> */}
+        <Route path='/FindJobs' element={<FindJobs />}></Route> */}
         {/* <Route path='*' element={<Error />}></Route> */}
         {/* <Route path='/About' element={<About />}></Route> */}
-        <Route path='/Profile' element={<Profile />}></Route>
+        {/* <Route path='/Profile' element={<Profile />}></Route> */}
       </Routes>
     </BrowserRouter>
 
     {/* <App /> */}
-    {/* <Login /> */}
+    <Login />
 
     {/* <FindJobs /> */}
 
