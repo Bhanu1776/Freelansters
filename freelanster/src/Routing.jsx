5faeacd1b170bb6944eef116146801ec7e6caaa5
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-11-01 12:43:37 +0530

    Added: All Frontend routing will exist here And added useContext and useReducer hook for Login/Logout Functionality And MultiRoutes also added -- Easiest way

---
+++ freelanster/src/Routing.jsx
@@ -0,0 +1,56 @@
+import React,{ createContext, useReducer }  from 'react'
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { initialState, reducer } from './reducer/UseReducer';
+import App from './App';
+import Login from './Login';
+import FindJobs from './FindJobs';
+import Logout from './Components/Logout';
+import FindFreelancer from './FindFreelancer';
+// import Profile from './Components/Profile';
+
+
+// Context API
+export const UserContext = createContext();
+
+
+const Paths = () => {
+  return (
+    <Routes>
+      <Route exact path='/' element={<App />}></Route>
+      {/* <Route path='/Home' element={<App />}></Route> */}
+      <Route path='Login' element={<Login />}></Route>
+      <Route path='/FindJobs' element={<FindJobs />}></Route>
+      <Route path='/FindJobs/Logout' element={<Logout />}></Route>
+      <Route path='/FindFreelancer' element={<FindFreelancer />}></Route>
+      <Route path='/FindFreelancer/Logout' element={<Logout />}></Route>
+      {/* <Route path='/Profile' element={<Profile />}></Route> */}
+      <Route path='/Logout' element={<Logout />}></Route>
+      {/* <Route path='*' element={<Error />}></Route> */}
+    </Routes>
+  )
+}
+
+const Routing = () => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <>
+      < UserContext.Provider value={{ state, dispatch }}>
+        <BrowserRouter>
+          <Paths />
+        </BrowserRouter>
+      </UserContext.Provider>
+      
+      {/* <App /> */}
+      {/* <Login /> */}
+
+      {/* <FindJobs /> */}
+      {/* <FindFreelancer/> */}
+
+      {/* <Error /> */}
+  </>
+  )
+  
+}
+
+export default Routing
