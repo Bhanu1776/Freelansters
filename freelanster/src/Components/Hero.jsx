d1f31d6a93bdb95351b09719fa0c15711715a08a
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-11-02 17:06:21 +0530

    Update: Added Props to change data dynamically and Fixed Width of Hero 'static'

--- freelanster/src/Components/Hero.jsx
+++ freelanster/src/Components/Hero.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import styled from 'styled-components';
 
 
-const Hero = () => {
+const Hero = (props) => {
   const Wrapper = styled.section`
     section.static {
   position: relative;
-  width: 98.9vw;
+  width: 99.3vw;
   height: 100vh;
   display: flex;
   justify-content: center;
@@ -16,7 +16,7 @@ const Hero = () => {
 section.static::before {
   content: " ";
   position: absolute;
-  background-image: url("https://images.unsplash.com/photo-1498354178607-a79df2916198?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1888&q=80");
+  background-image: ${props.img};
   background-repeat: no-repeat;
   background-position: center center;
   background-size: cover;
@@ -95,11 +95,9 @@ section.static h1 {
       <Wrapper>
         <section className="static">
           <div className="textArea">
-            <h1>Find Jobs</h1>
+            <h1>{ props.title}</h1>
             <br />
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati qui ducimus aliquid? Iste, impedit
-              nemo.
-            </p>
+            <p>{props.desc}</p>
             <div class="wrapper">
               <div class="searchBar">
                 <input id="searchQueryInput" type="text" name="searchQueryInput" placeholder="Find Jobs" />
