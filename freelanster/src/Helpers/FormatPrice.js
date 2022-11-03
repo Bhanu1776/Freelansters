d13510fb69fec0da275cfd215b6a99f12371c552
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-11-03 17:36:05 +0530

    Fix: Removed unused Imports Warnings resolved

--- freelanster/src/Helpers/FormatPrice.js
+++ freelanster/src/Helpers/FormatPrice.js
@@ -1,10 +1,9 @@
-import React from 'react';
 
-const FormatPrice=({price})=>{
-    return Intl.NumberFormat("en-IN",{
-        style:"currency",
-        currency:'INR',
-        maximumFractionDigits:2,
+const FormatPrice = ({ price }) => {
+    return Intl.NumberFormat("en-IN", {
+        style: "currency",
+        currency: 'INR',
+        maximumFractionDigits: 2,
     }).format(price);
 };
 
