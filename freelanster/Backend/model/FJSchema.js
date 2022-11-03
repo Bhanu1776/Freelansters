2fd1a6b6786a2c137c971de6049785f641fdebed
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-11-03 15:11:49 +0530

    Added: Database schema for FindJobs Cards

---
+++ freelanster/Backend/model/FJSchema.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+
+const FJSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+
+})
