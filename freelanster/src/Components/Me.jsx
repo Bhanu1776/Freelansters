b92d1d2ee4f466ca3da550bedec4a77e62215a06
Osama <shaikhosama499@gmail.com>
2022-11-04 10:57:40 +0530

    Update: Preloader added and Css Refactored

--- freelanster/src/Components/Me.jsx
+++ freelanster/src/Components/Me.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import Images from './Img/imgindex.js'
+import React, { useState } from 'react';
+// import Images from './Img/imgindex.js'
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
@@ -11,10 +13,20 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
+import Images from '../Img/imgindex';
 
 
 
 const Me = () => {
+
+    const [loading, setLoading] = useState(true);
+    const preloader = document.getElementById('preloader');
+    if (preloader) {
+        setTimeout(() => {
+            preloader.style.display = 'none';
+            setLoading(false);
+        }, 0);
+    }
 
     const Item = styled(Paper)(({ theme }) => ({
         backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -24,44 +36,79 @@ const Me = () => {
         color: theme.palette.text.secondary,
     }));
 
-    return (
-        <>
-            <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={2}>
-                    <Grid item xs={4}>
-                        <Item>
-                            <Card sx={{ minWidth: 275 }}>
-                                <CardContent>
-                                    <Stack direction="row" spacing={2}>
-                                        <Avatar
-                                            alt="Remy Sharp"
-                                            src="/static/images/avatar/1.jpg"
-                                            sx={{ width: 56, height: 56 }}
-                                        />
-                                    </Stack>
-                                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                                        Word of the Day
-                                    </Typography>
-                                    <Typography variant="body2">
-                                        Osama Shaikh
-                                    </Typography>
-                                </CardContent>
-                                <CardActions>
-                                    <Button size="small">Learn More</Button>
-                                </CardActions>
-                            </Card>
-                        </Item>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Item>xs=4</Item>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Item>xs=4</Item>
-                    </Grid>
-                </Grid>
-            </Box>
+    const styling = {
+        container: {
+            padding: 1,
+            marginTop: 14,
+            // flexGrow: 2,
+            width: "60%",
+        }
+    }
 
-        </>
+    return (
+        !loading && (
+
+            <>
+                <CssBaseline />
+                <Container sx={styling.container} fixed >
+
+
+                    <Box sx={{ flexGrow: 6 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={4}>
+                                {/* <Item> */}
+                                <Card sx={{ minWidth: 280 }}>
+                                    <CardContent>
+                                        <Stack direction="row" spacing={2}>
+                                            <Avatar
+                                                alt="osama"
+                                                src={Images.osama}
+                                                sx={{ width: 56, height: 56 }}
+                                            />
+                                        </Stack>
+                                        <Typography variant="body2">
+                                            Osama Shaikh
+                                        </Typography>
+                                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                            Full Stack Developer
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size="small" variant='contained'>Follow</Button>
+                                    </CardActions>
+                                </Card>
+                                {/* </Item> */}
+                            </Grid>
+                            <Grid item xs={8}>
+                                <Card sx={{ minWidth: 280 }}>
+                                    <Stack direction="row" spacing={2}>
+                                        <CardContent>
+
+
+                                            <Typography variant="body4">
+                                                Osama Shaikh
+                                            </Typography>
+                                            <br></br>
+                                            <Typography variant="body2" color={"#1cd6ce"} mt={2}>
+                                                Shaikhosama@gmail.com
+                                            </Typography>
+
+                                        </CardContent>
+                                    </Stack>
+                                    <CardActions>
+                                        <Button size="small" variant='contained'>Follow</Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Item>xs=4</Item>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Container>
+
+            </>
+        )
     );
 }
 
