9f6895f3fc2b5949007fede7a2b6cd3047dc7ef4
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-11-07 16:05:15 +0530

    Update: email and phone values has to be unique now

--- freelanster/Backend/model/userSchema.js
+++ freelanster/Backend/model/userSchema.js
@@ -10,11 +10,13 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     phone: {
         type: Number,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
