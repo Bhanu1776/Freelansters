058df0f66057a1abe4fcab7aa46b09a7fede1533
Bhanu Sunka <sunkabhanu281202@gmail.com>
2022-11-07 21:53:00 +0530

    Added: Authentication For FF

--- freelanster/src/FindFreelancer.js
+++ freelanster/src/FindFreelancer.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { Helmet } from "react-helmet";
 import Navbar from './Components/Navbar';
 import FindFreelan from './Components/FFlancer';
@@ -6,6 +7,8 @@ import Footer from './Components/Footer';
 
 
 const FindFreelancer = () => {
+
+    const navigate = useNavigate();
 
     const [loading, setLoading] = useState(true);
     const preloader = document.getElementById('preloader');
@@ -15,6 +18,36 @@ const FindFreelancer = () => {
             setLoading(false);
         }, 0);
     }
+
+    const callFF = async () => {
+
+        try {
+            const res = await fetch('/Findjobs', {
+                method: "GET",                        //! IMP
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                credentials: "include"
+            });
+
+            const data = await res.json();
+            console.log(data);
+
+            if (!res.status === 200) {
+                const error = new Error(res.error);
+                throw error;
+            }
+        } catch (err) {
+            console.log(err);
+            navigate('/Login')
+        }
+    }
+
+    useEffect(() => {
+        callFF();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return (
         !loading && (
