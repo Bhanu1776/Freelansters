e3ea22bc45c5a92e1b9c7d85e970cafcc8828dc8
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-02-01 17:55:47 +0530

    Misc: Changed localhost port number

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 dotenv.config({ path: './config.env' });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 require('./db/conn.js')
 
 const FJSchema = require('./model/FJSchema');
